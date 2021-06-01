fbfdd52b9ae0f820645971578963eb283a4cbefe
Onesimus Addo <38097150+draddo11@users.noreply.github.com>
2021-06-01 17:58:37 +0000

    files updated

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+<span>
+     
+     <h1  position = center
+      >
+       
+       Onesimus Addo Appiah
+        </h1>
+     <aside id='icons'>
+        
+      <a href="mailto:draddo11@gmail.com" >
+      <v-icon x-large>mdi-email</v-icon>
+      </a>
+       <a href="https://www.linkedin.com/in/onesimus-addo-appiah-1b7b78129/" target='_blank'>
+      <v-icon x-large >mdi-linkedin</v-icon>
+       </a>
+      <a href='https://github.com/draddo11' target='_blank' >
+      <v-icon x-large>mdi-github</v-icon>
+      </a>
+      <a href ='https://twitter.com/draddo11?lang=en' target='_blank' >
+      <v-icon x-large>mdi-twitter</v-icon>
+      </a>
+     </aside> 
+   </span>  
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data: () => ({
+    model: null,
+    
+  })
+}
+ 
+  
+</script>
